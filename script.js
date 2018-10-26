fe81765a49d90b97ecf97bf52e51bba93dc4e6d1
Luisa Arevalo <00038617@uca.edu.sv>
2018-10-26 13:34:03 -0600

    Ejercicio

---
+++ script.js
@@ -0,0 +1,72 @@
+// Object Literal
+var person = {
+    /* property: value*/
+    name: "Néstor",
+    lastname: "Aldana",
+    birthday: Date.now()
+}; // JSON (JavaScript Object Notation)
+
+// Access to propertys
+console.log(person.name);
+// Change object's property value
+person.birthday = new Date(1994, 0, 11); // 11 - Jan - 1994
+console.log(person.birthday);
+
+console.log(person.dui) // undefined
+person.dui = "000000000" // Assign 
+console.log(person.dui) // 000000000
+
+// Example
+function createPerson(name, lastname, birthday, dui) {
+    return {
+        name,
+        lastname,
+        birthday,
+        dui
+    }
+}
+
+let list = [] // To save persons
+
+// To Add 10 fake persons
+for (let i = 0; i < 10; i++) {
+    list.push(createPerson(`Name ${i}`, `Lastname ${i}`, new Date().setFullYear(1990 + i + Math.floor(Math.random() * 5)), `000000${i}`));
+}
+
+console.table(list);
+
+// Array Higher function
+// Show only the name persons
+console.table(list.map(({
+    name
+}) => name));
+
+// Get average age
+console.log("Average age %i", list.reduce((sum, {
+    birthday
+}) => {
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970) + sum;
+}, 0) / list.length);
+
+
+// More readable
+//===================== modificacion a fecha de nacimiento =============================
+function getAge(birthday) {
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+    //La unica modificacion que se realizara fue que si el mes era mayor que la fecha actual
+    //o si que el mes es el mismo y el dia mayor que la fecha actual, se le restara
+    //debido a que no los ha cumplido annios
+    if(getDate.getMonth > Date.now().getMonth || (ageDate.getMonth == Date.now().getMonth && ageDate.getDate > Date.now().getDate)){
+        return age-1;
+    }
+    
+    return age;
+}
+
+console.log("Average age %i", list.reduce((sum, {
+    birthday
+}) => getAge(birthday) + sum, 0) / list.length);
